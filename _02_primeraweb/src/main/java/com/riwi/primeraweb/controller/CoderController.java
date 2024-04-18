2c72f3530a009bfcae40fb40ba7f8224ee8b5ee6
danielsm <danielsanchezm92@gmail.com>
2024-04-18 09:09:26 -0500

    CRUD Th

--- _02_primeraweb/src/main/java/com/riwi/primeraweb/controller/CoderController.java
+++ _02_primeraweb/src/main/java/com/riwi/primeraweb/controller/CoderController.java
@@ -4,8 +4,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.riwi.primeraweb.entity.Coder;
 import com.riwi.primeraweb.services.CoderService;
@@ -27,4 +32,54 @@ public class CoderController {
         // Se debe retornar el nombre exacto de la vista HTML
         return "viewCoder";
     }
+
+    @GetMapping("/form")
+    public String showViewFormCoder(Model objModel){
+        objModel.addAttribute("coder", new Coder());
+        objModel.addAttribute("action", "/coder/create");
+        return "viewForm";
+    }
+
+    // Método para mostrar el formulario de actualizar un coder
+    @GetMapping("/update/{id}")
+    public String showFormUpdate(@PathVariable Long id, Model objModel){
+
+        Coder objCoderFind = this.objCoderService.findById(id);
+        objModel.addAttribute("coder", objCoderFind);
+        objModel.addAttribute("action", "/edit/"+id);
+
+        return "viewForm";
+    }
+
+    // Método para eliminar
+
+    // @GetMapping("/delete/{id}")
+    // public String deleteCoder(@PathVariable Long id, @ModelAttribute Coder obCoder){
+    //     this.objCoderService.delete(obCoder);
+    //     return "redirect:/";
+    // }
+
+    @GetMapping("/delete/{id}")
+    public String deleteCoder(@PathVariable Long id){
+        this.objCoderService.delete(id);
+        return "redirect:/";
+    }
+
+    // Método para actualizar
+    @PostMapping("/edit/{id}")
+    public String updateCoder(@PathVariable Long id, @ModelAttribute Coder objCoder){
+        this.objCoderService.update(id, objCoder);
+        return "redirect:/";
+    }
+
+    // Método para insertar un coder mediante el verbo POST
+    //@ModelAttribute se encarga de obtener la información enviada desde la vista
+    @PostMapping("/coder/create")
+    public String createCoder(@ModelAttribute Coder objCoder){
+        // LLamamos al servicio para que inserte enviándole un Coder a insertar
+        this.objCoderService.insert(objCoder);
+        return "redirect:/";
+    }
+
+
 }
--- _02_primeraweb/src/main/java/com/riwi/primeraweb/services/CoderService.java
+++ _02_primeraweb/src/main/java/com/riwi/primeraweb/services/CoderService.java
@@ -21,4 +21,44 @@ public class CoderService {
     public List<Coder> findAll(){
         return this.objCoderRepository.findAll();
     }
+
+    // Servicio para guardar un coder
+    public Coder insert(Coder objCoder){
+        return this.objCoderRepository.save(objCoder);
+    }
+
+    // Servicio para actualizar un coder
+    public Coder update(Long id, Coder objCoder){
+
+        // Buscar al coder con ese id
+        Coder objCoderDB = this.findById(id);
+
+        // Verificar que exista el coder
+        if(objCoderDB == null) return null;
+
+        // Actualizar el coder antiguo
+        objCoderDB = objCoder;
+
+        // Guardarlo
+        return this.objCoderRepository.save(objCoderDB);
+    }
+
+    
+    // Servicio para eliminar un coder
+    // public void delete(Coder objCoder){
+    //     // Eliminarlo
+    //     this.objCoderRepository.delete(objCoder);
+    // }
+
+    // Servicio para eliminar un coder
+    public void delete(Long id){
+        // Eliminarlo
+        this.objCoderRepository.deleteById(id);
+    }
+
+
+    public Coder findById(Long id){
+        return this.objCoderRepository.findById(id).orElse(null);
+    }
+
 }
